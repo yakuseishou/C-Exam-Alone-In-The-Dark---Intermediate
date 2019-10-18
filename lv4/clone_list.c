b2fdf700c1586abd75285d563942880f1b9b6e98
Kai Chen <kchen2@e1z2r5p6.42.us.org>
2019-10-18 03:35:59 -0700

    break into parts

--- lv4/clone_list.c
+++ lv4/clone_list.c
@@ -6,6 +6,8 @@ struct s_node {
 	struct s_node *other;
 };
 
+/* initiating a s_node */
+
 struct s_node *init(int v)
 {
 	struct s_node *new = malloc(sizeof(struct s_node));
@@ -14,6 +16,8 @@ struct s_node *init(int v)
 	new->other = 0;
 	return (new);
 }
+
+/* copying s_node->next */
 
 struct s_node	*clone_next(struct s_node *node, int *nodeL)
 {
@@ -25,26 +29,23 @@ struct s_node	*clone_next(struct s_node *node, int *nodeL)
 	return (new);
 }
 
-struct s_node *clone_list(struct s_node *node)
+/* copying s_node->other */
+
+struct s_node	*clone_other(struct s_node *node, struct s_node *clon, int nodeL)
 {
-	if (!node)
-		return (0);
-	struct s_node *tmp = node;
-	int		nodeL = 0;
-	struct s_node *clon = clone_next(node, &nodeL);
+	struct s_node 	*tmp = node;
 	struct s_node   *tmpC = clon;
 
 	struct s_node	**old = malloc(sizeof(struct s_node) * nodeL);
 	struct s_node   **ne = malloc(sizeof(struct s_node) * nodeL);
-	int		i = 0;
-	
-	while(tmp && tmpC)
+	int				i = 0;
+
+	for(i = 0; i < nodeL; i++)
 	{
 		old[i] = tmp;
 		ne[i] = tmpC;
 		tmp = tmp->next;
 		tmpC = tmpC->next;
-		i++;
 	}
 	tmp = node;
 	tmpC = clon;
@@ -52,72 +53,77 @@ struct s_node *clone_list(struct s_node *node)
 	{
 		if (tmp->other)
 		{
-			i = 0;
-			while(i < nodeL)
+			for (i = 0; i < nodeL; i++)
 			{
 				if (tmp->other != NULL && old[i] == tmp->other)
 				{
 					tmpC->other = ne[i];
 					break;
 				}
-				i++;
 			}
 		}
 		tmp = tmp->next;
 		tmpC = tmpC->next;
 	}
+	return (clon);
+}
+
+struct s_node *clone_list(struct s_node *node)
+{
+	if (!node)
+		return (0);
+	int		nodeL = 0;
+	struct s_node *clon = clone_next(node, &nodeL);
+	clone_other(node, clon, nodeL);
 
 	return (clon);
 }
-<<<<<<< HEAD
-=======
+
 /* main for testing */
-/*
-#include <stdio.h>
 
-int main() {
+// #include <stdio.h>
 
-    struct s_node *a = init(1);
-    struct s_node *b = init(2);
-    struct s_node *c = init(3);
-    struct s_node *d = init(4);
+// int main() {
 
-    a->next = b;
-    b->next = c;
-    c->next = d;
-    a->other = a;
-    b->other = b;
-    c->other = a;
-    d->other = b;
+//     struct s_node *a = init(1);
+//     struct s_node *b = init(2);
+//     struct s_node *c = init(3);
+//     struct s_node *d = init(4);
 
-    struct s_node *tmp = a;
-    while(tmp)
-    {
-        printf("%d ", tmp->data);
-        tmp = tmp->next;
-    }
-    tmp = a;
-    while(tmp)
-    {
-        printf("%d ", tmp->other->data);
-        tmp = tmp->next;
-    }
-    printf("\n");
-    struct s_node *new = clone_list(a);
-    tmp = new;
-    while(tmp)
-    {
-        printf("%d ", tmp->data);
-        tmp = tmp->next;
-    }
-    tmp = new;
-    while(tmp)
-    {
-        printf("%d ", tmp->other->data);
-        tmp = tmp->next;
-    }
-    printf("\n");
-    return 0;
-}
-*/
->>>>>>> 82dcc0c0fcc77f1cf1910a329a911b617659ecd3
+//     a->next = b;
+//     b->next = c;
+//     c->next = d;
+//     a->other = a;
+//     b->other = b;
+//     c->other = a;
+//     d->other = b;
+
+//     struct s_node *tmp = a;
+//     while(tmp)
+//     {
+//         printf("%d ", tmp->data);
+//         tmp = tmp->next;
+//     }
+//     tmp = a;
+//     while(tmp)
+//     {
+//         printf("%d ", tmp->other->data);
+//         tmp = tmp->next;
+//     }
+//     printf("\n");
+//     struct s_node *new = clone_list(a);
+//     tmp = new;
+//     while(tmp)
+//     {
+//         printf("%d ", tmp->data);
+//         tmp = tmp->next;
+//     }
+//     tmp = new;
+//     while(tmp)
+//     {
+//         printf("%d ", tmp->other->data);
+//         tmp = tmp->next;
+//     }
+//     printf("\n");
+//     return 0;
+// }
